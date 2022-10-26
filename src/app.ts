ec22155e04fade18500453ad767725bee3582738
Ahmed Gomaa <a.gomaa003@gmail.com>
2022-10-26 02:28:16 +0200

    refactor: Refactored the name of entry files

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+const port = 3000;
+
+app.use(routes);
+app.set('view engine', 'ejs');
+app.set('views', './view');
+
+
+
+app.listen(port, ()=> {
+    console.log(`server started at localhost:${port}`)
+   });
+
+export default app; 
