08bea940cc9b00d2b53ca6b3252e9cea751e1460
Ahmed Gomaa <a.gomaa003@gmail.com>
2022-10-26 02:25:04 +0200

    feat: Modified packages and config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,13 @@ module.exports = {
     commonjs: true,
     es2021: true
   },
-  extends: 'standard-with-typescript',
+  extends: ['standard-with-typescript', 'prettier'],
   overrides: [
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
   },
   rules: {
   }
